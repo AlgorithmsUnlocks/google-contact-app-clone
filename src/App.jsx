ae860022808c803036bea2cac4c1f3df670a8346
AlgorithmsUunlocks <algorithmsunlocks@gmail.com>
2022-09-16 15:45:48 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    
+    <div className="appFile">
+        <h3 className='text-center p-3'>
+            Authlab Google Contact Application Projects
+            <br />
+            <small>By Ruman Ahmed</small>
+        </h3>
+    </div>
+  )
+}
+export default App
